25cb9301988d24d38e082b265400758602a23eea
Henrique Martins <hmartins.dev@gmail.com>
2025-10-31 11:44:00 -0300

    feat: create cryptography module

---
+++ src/infra/cryptography/bcrypt-hasher.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@nestjs/common'
+
+import { compare, hash } from 'bcryptjs'
+
+import { HashGenerator } from '@/domain/forum/application/cryptography/hash-generator'
+import { HashComparer } from '@/domain/forum/application/cryptography/hash-comparer'
+
+@Injectable()
+export class BcryptHasher implements HashGenerator, HashComparer {
+  private HASH_SALT_LENGHT = 8
+
+  hash(plain: string): Promise<string> {
+    return hash(plain, this.HASH_SALT_LENGHT)
+  }
+
+  compare(plain: string, hash: string): Promise<boolean> {
+    return compare(plain, hash)
+  }
+}
