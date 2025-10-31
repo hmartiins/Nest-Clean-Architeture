1d12809262f1c5f6c326a048798284d414a00327
Henrique Martins <hmartins.dev@gmail.com>
2025-10-30 22:52:44 -0300

    chore: add Prettier and ESLint configuration files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,17 @@
 import rocketseatConfig from '@rocketseat/eslint-config/node.mjs'
+import eslintConfigPrettier from 'eslint-config-prettier'
 
 export default [
   ...rocketseatConfig,
+  eslintConfigPrettier,
   {
     ignores: ['node_modules', 'dist'],
   },
   {
     rules: {
+      'no-new': 'off',
       'no-useless-constructor': 'off',
+      '@stylistic/max-len': 'off', // Prettier handles this
     },
   },
 ]
