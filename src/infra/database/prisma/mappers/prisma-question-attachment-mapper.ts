067100073183fa83b65957d34b769f5d53d6f865
Henrique Martins <hmartins.dev@gmail.com>
2025-11-07 20:17:06 -0300

    feat: create question with attachment

--- src/infra/database/prisma/mappers/prisma-question-attachment-mapper.ts
+++ src/infra/database/prisma/mappers/prisma-question-attachment-mapper.ts
@@ -1,4 +1,4 @@
-import { Attachment as PrismaAttachment } from '@prisma/client'
+import { Prisma, Attachment as PrismaAttachment } from '@prisma/client'
 
 import { QuestionAttachment } from '@/domain/forum/enterprise/entities/question-attachment'
 import { UniqueEntityID } from '@/core/entities/unique-entity-id'
@@ -16,5 +16,22 @@ export class PrismaQuestionAttachmentMapper {
       },
       new UniqueEntityID(raw.id),
     )
+  }
+
+  static toPrismaUpdateMany(
+    attachments: QuestionAttachment[],
+  ): Prisma.AttachmentUpdateManyArgs {
+    const attachmentsIds = attachments.map((attachment) =>
+      attachment.attachmentId.toString(),
+    )
+
+    return {
+      where: {
+        id: {
+          in: attachmentsIds,
+        },
+      },
+      data: { questionId: attachments[0].questionId.toString() },
+    }
   }
 }
--- src/infra/http/controllers/create-question.controller.e2e-spec.ts
+++ src/infra/http/controllers/create-question.controller.e2e-spec.ts
@@ -10,23 +10,26 @@ import { JwtService } from '@nestjs/jwt'
 import { describe, expect, test, beforeAll } from 'vitest'
 import { DatabaseModule } from '@/infra/database/database.module'
 import { StudentFactory } from 'test/factories/make-student'
+import { AttachmentFactory } from 'test/factories/make-attachment'
 
 describe('Create Question (e2e)', () => {
   let app: INestApplication
   let prisma: PrismaService
   let jwt: JwtService
   let studentFactory: StudentFactory
+  let attachmentFactory: AttachmentFactory
 
   beforeAll(async () => {
     const module = await Test.createTestingModule({
       imports: [AppModule, DatabaseModule],
-      providers: [StudentFactory],
+      providers: [StudentFactory, AttachmentFactory],
     }).compile()
 
     app = module.createNestApplication()
     prisma = module.get(PrismaService)
     jwt = module.get(JwtService)
     studentFactory = module.get(StudentFactory)
+    attachmentFactory = module.get(AttachmentFactory)
 
     await app.init()
   })
@@ -36,12 +39,16 @@ describe('Create Question (e2e)', () => {
 
     const accessToken = jwt.sign({ sub: student.id.toString() })
 
+    const attachment1 = await attachmentFactory.makePrismaAttachment()
+    const attachment2 = await attachmentFactory.makePrismaAttachment()
+
     const response = await request(app.getHttpServer())
       .post('/questions')
       .set('Authorization', `Bearer ${accessToken}`)
       .send({
         title: 'New Question',
         content: 'This is a new question',
+        attachments: [attachment1.id.toString(), attachment2.id.toString()],
       })
 
     expect(response.status).toBe(201)
@@ -51,5 +58,19 @@ describe('Create Question (e2e)', () => {
     })
 
     expect(questionOnDatabase).toBeTruthy()
+
+    const attachmentsOnDatabase = await prisma.attachment.findMany({
+      where: {
+        questionId: questionOnDatabase?.id,
+      },
+    })
+
+    expect(attachmentsOnDatabase).toHaveLength(2)
+    expect(attachmentsOnDatabase).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({ id: attachment1.id.toString() }),
+        expect.objectContaining({ id: attachment2.id.toString() }),
+      ]),
+    )
   })
 })
