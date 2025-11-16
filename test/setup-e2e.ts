89a17f0442f554502a95e25c969112ee221ed9c3
Henrique Martins <hmartins.dev@gmail.com>
2025-11-15 22:03:34 -0300

    feat: e2e setup with Redis integration for test environment

--- test/setup-e2e.ts
+++ test/setup-e2e.ts
@@ -1,14 +1,24 @@
 import { config } from 'dotenv'
+
+import { Redis } from 'ioredis'
 import { PrismaClient } from '@prisma/client'
 import { randomUUID } from 'node:crypto'
 import { execSync } from 'node:child_process'
 import { beforeAll, afterAll } from 'vitest'
 import { DomainEvents } from '@/core/events/domain-events'
+import { envSchema } from '@/infra/env/env'
 
 config({ path: '.env', override: true })
 config({ path: '.env.test', override: true })
 
+const env = envSchema.parse(process.env)
+
 const prisma = new PrismaClient()
+const redis = new Redis({
+  host: env.REDIS_HOST,
+  port: env.REDIS_PORT,
+  db: env.REDIS_DB,
+})
 
 function generateUniqueDatabaseURL(schemaId: string) {
   if (!process.env.DATABASE_URL) {
@@ -29,11 +39,14 @@ beforeAll(async () => {
 
   process.env.DATABASE_URL = databaseURL
 
+  DomainEvents.shouldRun = false
+
+  await redis.flushdb()
+
   execSync('npx prisma migrate deploy')
 })
 
 afterAll(async () => {
   await prisma.$executeRawUnsafe(`DROP SCHEMA IF EXISTS "${schemaId}" CASCADE`)
   await prisma.$disconnect()
-  DomainEvents.shouldRun = false
 })
